1d8e6a56e0f469974c7ef888244fa77a02f13485
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-12-17 20:03:58 -0500

    inicio del proyecto de peliculas con spring y thymeleaf

---
+++ src/test/java/com/sistema/ecpeli/EcpeliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.ecpeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcpeliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
